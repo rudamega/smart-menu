cebec524614ebe7d5a03dc13f6ecb37c19dd3379
ruben medina <ruben@megachopp.com.py>
2020-06-09 13:57:32 -0400

    pequenas modificaoes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,17 @@
 class ItemsController < ApplicationController
-  def index
-  end
-
   def show
   end
 
   def new
+    @item = Item.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.restaurant_id = params[:restaurant_id]
+    @item.save
+    redirect_to restaurant_path(@item.restaurant_id)
   end
 
   def edit
@@ -18,6 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category, :price)
   end
 
 end
