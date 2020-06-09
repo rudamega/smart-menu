bc7d1ba99886efc930dcce61803067c4efa3fbff
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-06-09 17:07:20 -0300

    added cart_items controller and methods

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,22 +1,26 @@
 class CartItemsController < ApplicationController
   def index
+    @cart_items = Cart_item.where(user: current_user).where(status: added)
   end
 
-  def show
-  end
-
-  def new
+  def news
+    @cart_item = Cart_item.new
   end
 
   def create
+    @cart_item = Cart_item.new(cart_item_params)
   end
 
-  def edit
-  end
-
-  def update
-  end
 
   def destroy
+    @cart_item = Cart_item.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :cart_id, :quantity, :status)
   end
 end
