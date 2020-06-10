d3a9ce86f6df2ac3398a85ac16800581242c7af9
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-06-10 20:42:35 -0300

    abertura da conta e adicionar um item no carrinho

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,4 @@
 class Cart < ApplicationRecord
   belongs_to :place
-  belongs_to :order
   belongs_to :user
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :items
+  has_many :places
 end
