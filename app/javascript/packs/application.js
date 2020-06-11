c777893b05d2d9560c0f9ae31ecf444bce6d5c44
antonio braz <antoniovbraz@gmail.com>
2020-06-11 12:17:26 -0300

    revert merge c51c060

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,14 +34,3 @@ document.addEventListener('turbolinks:load', () => {
   console.log("oiko");
   cartsPrueba();
 });
-
-/* JS for responsive bottom menu */
-/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
